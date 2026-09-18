f0977b3e7ec7aebddc739bad74592df70ac12acf
xii01 <rahulsagarxii01@gmail.com>
2026-09-18 21:26:33 +0530

    p7 TCPServer.java

---
+++ p7TCPServer.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPServer {
+    public static void main(String args[]) throws Exception {
+        ServerSocket sersock = new ServerSocket(4000);
+        System.out.println("Server ready for connection");
+
+        Socket sock = sersock.accept();
+        System.out.println("Connection is successful and waiting for chatting");
+
+        InputStream istream = sock.getInputStream();
+        BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+        String fname = fileRead.readLine();
+
+        BufferedReader contentRead = new BufferedReader(new FileReader(fname));
+
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+
+        String str;
+        while ((str = contentRead.readLine()) != null) {
+            pwrite.println(str);
+        }
+
+        contentRead.close();
+        pwrite.close();
+        fileRead.close();
+        sock.close();
+        sersock.close();
+    }
+}
